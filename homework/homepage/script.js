8e25c883c89a2e19f4d4e80b774a8e6ceb93d180
Johnathan Galbreath <johngalbreath1997@gmail.com>
2023-11-10 00:36:55 -0800

    added working scroll wheel increments and experimental touchscreen control

--- homework/homepage/script.js
+++ homework/homepage/script.js
@@ -1,29 +1,66 @@
 const carousel = document.getElementById('carousel');
 const items = document.querySelectorAll('.carousel-item');
-const itemWidth = items[0].offsetWidth;
-
-// Set the default selected index (e.g., the first item)
 let currentIndex = 2;
+let startX;
 
-// Function to update the selected item's styles
 function setSelectedItem(index) {
   items.forEach((item) => {
     item.classList.remove('selected');
-    
-    item.style.transform = 'scale(0.9)'; // Reset the scale for all items
+    item.style.transform = 'scale(0.9)';
   });
   items[index].classList.add('selected');
-  items[index].style.transform = 'scale(1.1)'; // Scale the selected item by 10%
+  items[index].style.transform = 'scale(1.1)';
 }
 
-// Set the default selected item when the page loads
-setSelectedItem(currentIndex);
+function handleSwipeStart(event) {
+  startX = event.touches[0].clientX;
+}
+
+function handleSwipeMove(event) {
+  event.preventDefault();
+}
+
+function handleSwipeEnd(event) {
+  const endX = event.changedTouches[0].clientX;
+  const deltaX = endX - startX;
+
+  if (deltaX > 50) {
+    // Swipe right, go to the previous item
+    currentIndex = Math.max(0, currentIndex - 1);
+  } else if (deltaX < -50) {
+    // Swipe left, go to the next item
+    currentIndex = Math.min(items.length - 1, currentIndex + 1);
+  }
+
+  setSelectedItem(currentIndex);
+}
+
+function handleMouseWheel(event) {
+  event.preventDefault();
+  currentIndex += event.deltaY > 0 ? 1 : -1;
+
+  if (currentIndex < 0) {
+    currentIndex = items.length - 1;
+  }
+
+  if (currentIndex >= items.length) {
+    currentIndex = 0;
+  }
+
+  setSelectedItem(currentIndex);
+}
+
+carousel.addEventListener('touchstart', handleSwipeStart);
+carousel.addEventListener('touchmove', handleSwipeMove);
+carousel.addEventListener('touchend', handleSwipeEnd);
+
+carousel.addEventListener('wheel', handleMouseWheel);
 
 items.forEach((item, index) => {
   item.addEventListener('click', () => {
-    setSelectedItem(index); // Update styles when an item is clicked
+    setSelectedItem(index);
   });
 });
 
-
-
+// Set the default selected item when the page loads
+setSelectedItem(currentIndex);
