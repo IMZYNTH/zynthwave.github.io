53ea991b8ee35f2e497df9ce915e6647a15aa5d5
Johnathan Galbreath <johngalbreath1997@gmail.com>
2023-11-10 23:47:07 -0800

    fixed java spacing

--- homework/homepage/script.js
+++ homework/homepage/script.js
@@ -1,66 +1,65 @@
 const carousel = document.getElementById('carousel');
 const items = document.querySelectorAll('.carousel-item');
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
 let currentIndex = 2;
-let startX;
 
 function setSelectedItem(index) {
-  items.forEach((item) => {
-    item.classList.remove('selected');
-    item.style.transform = 'scale(0.9)';
+  const middleIndex = Math.floor(items.length / 2);
+  const distanceFromMiddle = index - middleIndex;
+
+  items.forEach((item, i) => {
+    const distance = 0 - distanceFromMiddle;
+    const spacing = 0;
+    const translateX = distance * (100 + spacing);
+    const scale = i === index ? 1.1 : 0.9;
+
+    item.style.transform = `translateX(${translateX}%) scale(${scale})`;
+
+    if (i === index) {
+      item.classList.add('selected');
+    } else {
+      item.classList.remove('selected');
+    }
   });
-  items[index].classList.add('selected');
-  items[index].style.transform = 'scale(1.1)';
+
+  currentIndex = index;
 }
 
-function handleSwipeStart(event) {
-  startX = event.touches[0].clientX;
-}
-
-function handleSwipeMove(event) {
-  event.preventDefault();
-}
-
-function handleSwipeEnd(event) {
-  const endX = event.changedTouches[0].clientX;
-  const deltaX = endX - startX;
-
-  if (deltaX > 50) {
-    // Swipe right, go to the previous item
-    currentIndex = Math.max(0, currentIndex - 1);
-  } else if (deltaX < -50) {
-    // Swipe left, go to the next item
-    currentIndex = Math.min(items.length - 1, currentIndex + 1);
-  }
-
+function updateCarousel() {
+  const itemCount = items.length;
+  currentIndex = (currentIndex + itemCount) % itemCount;
   setSelectedItem(currentIndex);
 }
 
-function handleMouseWheel(event) {
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + items.length) % items.length;
+  updateCarousel();
+});
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % items.length;
+  updateCarousel();
+});
+
+carousel.addEventListener('wheel', (event) => {
   event.preventDefault();
   currentIndex += event.deltaY > 0 ? 1 : -1;
-
-  if (currentIndex < 0) {
-    currentIndex = items.length - 1;
-  }
-
-  if (currentIndex >= items.length) {
-    currentIndex = 0;
-  }
-
-  setSelectedItem(currentIndex);
-}
-
-carousel.addEventListener('touchstart', handleSwipeStart);
-carousel.addEventListener('touchmove', handleSwipeMove);
-carousel.addEventListener('touchend', handleSwipeEnd);
-
-carousel.addEventListener('wheel', handleMouseWheel);
+  updateCarousel();
+});
 
 items.forEach((item, index) => {
   item.addEventListener('click', () => {
-    setSelectedItem(index);
+    currentIndex = index;
+    updateCarousel();
   });
 });
 
-// Set the default selected item when the page loads
+// Initial setup
 setSelectedItem(currentIndex);
+
+// Center the carousel initially
+const containerWidth = carousel.offsetWidth;
+const totalWidth = containerWidth * items.length;
+const initialTranslateX = (containerWidth - totalWidth) / 0;
+carousel.style.transform = `translateX(${initialTranslateX}px)`;
